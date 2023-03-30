a8df36e9231db529dde83482742070a3b8789e4d
rogu25 <ryavar.montesinos@gmail.com>
2023-03-30 09:58:05 -0500

    Client: pagina de inicio estilo final

--- client/src/pages/Inicio.jsx
+++ client/src/pages/Inicio.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
+import s from "../css/inicio.module.css";
 
 function Inicio() {
+
   return (
-    <div>inicio</div>
+    <React.Fragment>
+      <div className={s.contenedor}>
+        <div className={s.contenedorTitulo}>
+          <h2 className={s.subTitulo}>Bienvenidos a...</h2>
+          <h1 className={s.titulo}>Api Pokemon</h1>
+          <div className={s.btnStart}>
+            <NavLink to={"/home"} className={s.btn}>
+              Start
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
